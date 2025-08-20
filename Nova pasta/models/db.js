95a05b866246a83972c86530f66544c5d97ae5c5
yaguimn <161677413+yaguimn@users.noreply.github.com>
2025-08-19 22:04:36 -0300

    Delete Nova pasta directory

--- Nova pasta/models/db.js
+++
@@ -1,6 +0,0 @@
-import Sequelize from 'sequelize';
-const sequelize = new Sequelize('restaurante', 'root', 'ifsp', {
-   host: "localhost",
-   dialect: 'mysql'
-});
-export { Sequelize, sequelize };
