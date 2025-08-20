2222df6b2ef567a94b59570d6544e632fdb6b425
yaguimn <161677413+yaguimn@users.noreply.github.com>
2025-08-19 22:34:13 -0300

    Add files via upload

---
+++ web2tri/restaurante2tri/models/MenuModel.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
+class MenuModel {
+    constructor() {
+        this.filePath = path.join(__dirname, '../data/cardapio.json');
+    }
+    async getMenu() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(this.filePath, 'utf8', (err, data) => {
+                if (err) {
+                    return reject(err);
+                }
+                try {
+                    const produtos = JSON.parse(data);
+                    resolve(produtos);
+                } catch (parseError) {
+                    reject(parseError);
+                }
+            });
+        });
+    }
+}
+
+module.exports = MenuModel;
